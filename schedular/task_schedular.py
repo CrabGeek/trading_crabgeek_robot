8ab2062d56e031eb811be342d826c8eaf29e6e33
Guipengtt <xgp980303@gmail.com>
2023-10-28 22:40:46 +0800

    update

--- schedular/task_schedular.py
+++ schedular/task_schedular.py
@@ -20,7 +20,8 @@ def init_glable_schedular(glabal_namespace: Namespace, config: BaseConfig):
                             config.get_market_kline_limit())
     schedular.add_job(global_tasks.schedular_get_market_condition_each_symbol,
                       'cron', 
-                      minute='*/15', 
+                    #   minute='*/15',
+                      minute = '0, 15, 30, 45',
                       args=[glabal_namespace.symbol_list, 
                             glabal_namespace.symbol_market,
                             glabal_namespace.symbol_market_data_update_event,
